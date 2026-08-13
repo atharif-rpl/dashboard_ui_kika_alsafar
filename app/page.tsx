0098a2e09add08d524986c658063c21b154852f1
atharif-rpl <atha55733@gmail.com>
2026-08-13 23:28:48 +0700

    change direction

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function RootPage() {
   // Otomatis mengarahkan user ke halaman login saat membuka root url (/)
-  redirect("/auth/login");
+  redirect("/login");
 }
